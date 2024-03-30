9b2462c31bb17a59b4e9591a661df2a496d0708f
Albert <gadiev6667@gmail.com>
2024-03-30 17:50:29 +0300

    vector

--- src/containners/s21_vector.cc
+++ src/containners/s21_vector.cc
@@ -1,188 +1,110 @@
-
-
-#include "s21_vector.h"
-
+#include "s21_stack.h"
 namespace s21 {
-// Constructors
-template <class value_type>
-vector<value_type>::vector() : size_(0), buf_(0) {}
+// CONSTRUCTORS
+template <typename value_type>
+stack<value_type>::stack() : head_(nullptr), size_(0) {}
 
-template <class value_type>
-vector<value_type>::vector(size_type n) {
-  vector_ = new value_type[n];
-  size_ = n;
-  buf_ = n;
-}
-
-template <class value_type>
-vector<value_type>::vector(std::initializer_list<value_type> const &items) {
+template <typename value_type>
+stack<value_type>::stack(std::initializer_list<value_type> const &items) {
   for (auto iter = items.begin(); iter != items.end(); ++iter) {
-    push_back(*iter);
+    push(*iter);
   }
 }
 
-template <class value_type>
-vector<value_type>::vector(const vector &v) {
-  vector_ = new value_type[v.buf_];
-  *this = v;
+template <typename value_type>
+stack<value_type>::stack(const stack &s) {
+  if (s.head_ == nullptr) {
+    head_ = nullptr;
+    return;
+  }
+  value_type array[s.size()];
+  int i = 0;
+  Node *temp = s.head_;
+  while (temp) {
+    array[i] = temp->value_;
+    temp = temp->prev_;
+    i++;
+  }
+  while (i > 0) {
+    push(array[i - 1]);
+    i--;
+  }
 }
 
-template <class value_type>
-vector<value_type>::vector(vector &&v) {
-  *this = std::move(v);
+template <typename value_type>
+stack<value_type>::stack(stack &&s) {
+  head_ = s.head_;
+  size_ = s.size_;
+  s.head_ = nullptr;
+  s.size_ = 0;
 }
 
-// Destructor
-template <class value_type>
-vector<value_type>::~vector() {
-  delete[] vector_;
-  size_ = 0;
-  buf_ = 0;
-  vector_ = nullptr;
+// DESTRUCTOR
+template <typename value_type>
+stack<value_type>::~stack() {
+  while (this->empty()) {
+    this->pop();
+  }
 }
 
-// Oper-r
-template <class value_type>
-vector<value_type> &vector<value_type>::operator=(const vector &v) noexcept {
-  std::copy(v.vector_, v.vector_ + v.size_, vector_);
-  size_ = v.size_;
-  buf_ = v.buf_;
-  return *this;
-}
-
-template <class value_type>
-vector<value_type> &vector<value_type>::operator=(vector &&v) noexcept {
-  if (this != &v) {
-    for (size_t i = 0; i < v.size(); ++i) {
-      push_back(v.vector_[i]);
-      v.vector_[i] = 0;
-    }
-    v.size_ = 0, v.buf_ = 0;
-    delete[] v.vector_;
-    v.vector_ = nullptr;
+// OPERATOR
+template <typename value_type>
+stack<value_type> &stack<value_type>::operator=(stack &&s) {
+  if (this != &s) {
+    this->~stack();
+    head_ = s.head_;
+    size_ = s.size_;
+    s.head_ = nullptr;
+    s.size_ = 0;
   }
   return *this;
 }
 
-template <class value_type>
-typename vector<value_type>::reference vector<value_type>::operator[](size_type pos) {
-  return vector_[pos];
+// FUNCTIONS
+template <typename value_type>
+typename stack<value_type>::const_reference stack<value_type>::top() {
+  if (this->empty()) {
+    throw "Stack is empty";
+  }
+
+  return head_->value_;
 }
 
-// Iter
-template <class value_type>
-typename vector<value_type>::iterator vector<value_type>::begin() {
-  return size_ > 0 ? &vector_[0] : 0;
+template <typename value_type>
+bool stack<value_type>::empty() {
+  return head_ == nullptr;
 }
 
-template <class value_type>
-typename vector<value_type>::iterator vector<value_type>::end() {
-  return size_ > 0 ? &vector_[size_] : 0;
-}
-
-template <class value_type>
-typename vector<value_type>::iterator vector<value_type>::insert(iterator pos, const_reference value) {
-  size_type position = pos - &vector_[0];
-  vector_[position] = value;
-  return &vector_[position];
-}
-
-// Func-s
-template <class value_type>
-typename vector<value_type>::reference vector<value_type>::at(size_type pos) {
-  if (pos >= buf_) throw std::out_of_range("Out of range");
-  return vector_[pos];
-}
-
-template <class value_type>
-typename vector<value_type>::const_reference vector<value_type>::front() {
-  if (empty()) throw "Is empty";
-  return vector_[0];
-}
-
-template <class value_type>
-typename vector<value_type>::const_reference vector<value_type>::back() {
-  if (empty()) throw "Is empty";
-  return vector_[size_ - 1];
-}
-
-template <class value_type>
-typename vector<value_type>::iterator vector<value_type>::data() {
-  if (empty()) throw "Is empty";
-  return &vector_[0];
-}
-
-template <class value_type>
-bool vector<value_type>::empty() {
-  return vector_ == nullptr ? true : false;
-}
-
-template <class value_type>
-typename vector<value_type>::size_type vector<value_type>::size() {
+template <typename value_type>
+typename stack<value_type>::size_type stack<value_type>::size() const {
   return size_;
 }
 
-template <class value_type>
-typename vector<value_type>::size_type vector<value_type>::max_size() {
-  return std::numeric_limits<size_type>::max() / sizeof(value_type) / 2;  //max()-bit sizeof-byte -> / 2
-}
-
-template <class value_type>
-typename vector<value_type>::size_type vector<value_type>::capacity() {
-  return buf_;
+template <typename value_type>
+void stack<value_type>::push(const_reference value) {
+  Node *ptr = new Node(value);
+  if (head_) {
+    ptr->prev_ = head_;
+    head_ = ptr;
+  } else
+    head_ = ptr;
+  ++size_;
 }
 
 template <typename value_type>
-void vector<value_type>::shrink_to_fit() {
-  value_type *new_vector_ = new value_type[size_];
-  std::copy(vector_, vector_ + size_, new_vector_);
-  delete[] vector_;
-  vector_ = new_vector_;
-  buf_ = size_;
-}
-
-template <class value_type>
-void vector<value_type>::clear() {
-  size_ = 0;
-}
-
-template <class value_type>
-void vector<value_type>::erase(iterator pos) {
-  if (pos > end()) pos = end();
-  if (pos < begin()) pos = begin();
-  for (iterator i = pos; i < end(); ++i) {   //{1,2,3,4,5} => {1,2,3->4,4->5} 
-    *i = *(i + 1);
+void stack<value_type>::pop() {
+  if (head_ != nullptr) {
+    Node *ptr = head_->prev_;
+    delete head_;
+    head_ = ptr;
+    --size_;
   }
-  size_--;
-}
-
-template <class value_type>
-void vector<value_type>::push_back(const_reference value) {
-  if (size_ == buf_) reserve(buf_ ? buf_ * 2 : 1);
-  vector_[size_++] = value;
 }
 
 template <typename value_type>
-void vector<value_type>::reserve(size_type size) { 
-  if (size > buf_) {
-    value_type *new_vector_ = new value_type[size];
-    std::copy(vector_, vector_ + size_, new_vector_);
-    delete[] vector_;
-    vector_ = new_vector_;
-    buf_ = size;
-  }
-}
-
-template <class value_type>
-void vector<value_type>::pop_back() {
-  size_--;
-}
-
-template <class value_type>
-void vector<value_type>::swap(vector &other) {
-  std::swap(vector_, other.vector_);
+void stack<value_type>::swap(stack &other) {
+  std::swap(head_, other.head_);
   std::swap(size_, other.size_);
-  std::swap(buf_, other.buf_);
 }
-
 }  // namespace s21
+
--- src/containners/s21_vector.h
+++ src/containners/s21_vector.h
@@ -15,43 +15,44 @@ class vector {
   using size_type = std::size_t;
 
  public:
-  // CONSTRUCTORS
+  // Construct
   vector();
   vector(size_type n);
   vector(std::initializer_list<value_type> const &items);
   vector(const vector &v);
   vector(vector &&v);
 
-  // DESTRUCTOR
+  // Dest
   ~vector();
 
-  // OPERATOR
-  vector &operator=(const vector &v) noexcept;
-  vector &operator=(vector &&v) noexcept;
-  reference operator[](size_type pos);
-
-  // ITERATORS
+  // Iter
   iterator begin();
   iterator end();
   iterator insert(iterator pos, const_reference value);
 
-  // FUNCTIONS
-  reference at(size_type pos);
-  const_reference front();
-  const_reference back();
-  T *data();
-  bool empty();
+  // Operator
+  vector &operator=(const vector &v) noexcept;
+  vector &operator=(vector &&v) noexcept;
+  reference operator[](size_type pos);
+
+
+  // Func-s
+  reference at(size_type pos);  
+  const_reference front();  
+  const_reference back();  
+  T *data();  
+  bool empty(); 
   size_type size();
-  size_type max_size();
-  void reserve(size_type size);
+  size_type max_size(); 
+  void reserve(size_type size); 
   size_type capacity();
   void shrink_to_fit();
   void clear();
-  void erase(iterator pos);
+  void erase(iterator pos); //delete by pos
   void push_back(const_reference value);
   void pop_back();
   void swap(vector &other);
-
+//fields
  private:
   size_type size_ = 0;
   size_type buf_ = 0;
