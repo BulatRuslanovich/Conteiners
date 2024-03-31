993c13190c2a612d1a1008a5666b2e8805568e52
bronwyns <bronwyns@student.21-school.ru>
2024-03-31 13:31:29 +0300

    first push

--- src/s21_containers.h
+++ src/s21_containers.h
@@ -1,6 +1,6 @@
 #ifndef SRC_S21_CONTAINERS_H
 #define SRC_S21_CONTAINERS_H
 
-#include "containers/s21_list.h"
+#include "s21_cont/s21_queue.h"
 
-#endif // SRC_S21_CONTAINERS_H
+#endif  // SRC_S21_CONTAINERS_H
