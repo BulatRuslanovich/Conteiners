136be718ef6e78fba14cc8eeb50daf9853368e5c
Albert <gadiev6667@gmail.com>
2024-04-06 17:42:44 +0300

    all

--- src/s21_containers.h
+++ src/s21_containers.h
@@ -1,6 +1,6 @@
 #ifndef SRC_S21_CONTAINERS_H
 #define SRC_S21_CONTAINERS_H
 
-#include "containners/s21_stack.h"
-
+// #include "containners/s21_stack.h"
+#include "containners/s21_array.h"
 #endif // SRC_S21_CONTAINERS_H
